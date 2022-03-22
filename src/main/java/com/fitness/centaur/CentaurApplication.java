8c0266df9a209f4722ff64e909bda64b06fe6cc0
DavidCassiet <davidleonelcassiet@hotmail.com>
2022-03-21 21:33:41 -0300

    project start

---
+++ src/main/java/com/fitness/centaur/CentaurApplication.java
@@ -0,0 +1,13 @@
+package com.fitness.centaur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentaurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentaurApplication.class, args);
+	}
+
+}
