5650c902abae3e100fc718e711fe440d9117461c
DavidCassiet <davidleonelcassiet@hotmail.com>
2022-03-24 20:17:41 -0300

    modification of ExercisePDTO

--- src/main/java/com/fitness/centaur/dto/ExercisePDTO.java
+++ src/main/java/com/fitness/centaur/dto/ExercisePDTO.java
@@ -7,6 +7,7 @@ import java.io.Serializable;
 @Data
 public class ExercisePDTO implements Serializable {
 
+    private Long id;
     private String exercise;
     private Integer seasons;
     private Integer repetitions;
